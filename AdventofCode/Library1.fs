16da3c8f75c551f24a85fab533f2cfaf7ba84b9c
interplinternet <astro@airmail.cc>
2016-12-19 18:14:58 -0800

    New solution, types, wrong answer.
    Adds newPos that takes a state and distance and gives a new coordinate.
    findBaseWithCoords now uses a Seq.fold instead of let rec.
    Updated examples to new types instead of using strings.
    
    Gives 303, which is the wrong answer, but answers correctly for the examples. Uses an active pattern to match headings as NESW instead of just a tuple.

--- AdventofCode/Library1.fs
+++ AdventofCode/Library1.fs
@@ -1,6 +1,4 @@
-﻿module day1
-
-open System
+﻿module Day1
 open System.Text.RegularExpressions
 
 type Turn = 
@@ -13,63 +11,82 @@ type Instruction =
 
 type State = 
     { pos : int * int
-      facing : int }
+      heading : int * int }
 
-let dir = "L2, L3, L3, L4, R1, R2, L3, R3, R3, L1, L3, R2, R3, L3, R4, R3, R3, L1, L4, R4, L2, R5, R1, 
-        L5, R1, R3, L5, R2, L2, R2, R1, L1, L3, L3, R4, R5, R4, L1, L189, L2, R2, L5, R5, R45, L3, R4, 
-        R77, L1, R1, R194, R2, L5, L3, L2, L1, R5, L3, L3, L5, L5, L5, R2, L1, L2, L3, R2, R5, R4, L2, 
-        R3, R5, L2, L2, R3, L3, L2, L1, L3, R5, R4, R3, R2, L1, R2, L5, R4, L5, L4, R4, L2, R5, L3, L2, 
-        R4, L1, L2, R2, R3, L2, L5, R1, R1, R3, R4, R1, R2, R4, R5, L3, L5, L3, L3, R5, R4, R1, L3, R1, 
-        L3, R3, R3, R3, L1, R3, R4, L5, L3, L1, L5, L4, R4, R1, L4, R3, R3, R5, R4, R3, R3, L1, L2, R1, 
-        L4, L4, L3, L4, L3, L5, R2, R4, L2"
+let initState = 
+    { pos = 0, 0
+      heading = 0, 1 }
 
-let rmatches = 
-    Regex.Matches(dir, "([LR])|(\\d+)") // match L, R, or any digit (not commas or spaces)
-    |> Seq.cast<Match> // originally of type MatchCollection but can be cast to a Seq<'a>
-    |> Seq.map (fun m -> m.Value) // MatchCollection objects have Captures, Groups, Index, Length, Success, and Value members
-    |> Seq.collect (function 
-           | "L" -> Turn Left
-           | "R" -> [ Turn Right ]
-           | n -> Move(Convert.ToInt32(n))
+// initial heading represents facing 0. Simply add the heading for every movement in a direction.
+// West is -1, 0; east is 1, 0; south is 0, -1
+let north, east, south, west = (0, 1), (1, 0), (0, -1), (-1, 0)
+
+let (|North|East|South|West|) = 
+    function 
+    | (0, 1) -> North
+    | (1, 0) -> East
+    | (0, -1) -> South
+    | (-1, 0) -> West
+
+let rotate facing dir = 
+    match dir, facing with
+    | Left, North -> west
+    | Left, East -> north
+    | Left, South -> east
+    | Left, West -> south
+    | Right, North -> east
+    | Right, East -> south
+    | Right, South -> west
+    | Right, West -> west
+
+let dir = 
+    "L2, L3, L3, L4, R1, R2, L3, R3, R3, L1, L3, R2, R3, L3, R4, R3, R3, L1, L4, R4, L2, R5, R1, L5, R1, R3, L5, R2, L2, R2, R1, L1, L3, L3, R4, R5, R4, L1, L189, L2, R2, L5, R5, R45, L3, R4, R77, L1, R1, R194, R2, L5, L3, L2, L1, R5, L3, L3, L5, L5, L5, R2, L1, L2, L3, R2, R5, R4, L2, R3, R5, L2, L2, R3, L3, L2, L1, L3, R5, R4, R3, R2, L1, R2, L5, R4, L5, L4, R4, L2, R5, L3, L2, R4, L1, L2, R2, R3, L2, L5, R1, R1, R3, R4, R1, R2, R4, R5, L3, L5, L3, L3, R5, R4, R1, L3, R1, L3, R3, R3, R3, L1, R3, R4, L5, L3, L1, L5, L4, R4, R1, L4, R3, R3, R5, R4, R3, R3, L1, L2, R1, L4, L4, L3, L4, L3, L5, R2, R4, L2"
 
 let directions = 
-    dir.Split(',')
-    |> Array.toList
-    |> List.map (fun s -> s.Trim())
+    Regex.Matches(dir, @"([LR])|(\d+)") // match L, R, or any digit (not commas or spaces)
+    |> Seq.cast<Match> // originally of type MatchCollection but IEnumerable can be cast to a Seq<'a>
+    |> Seq.map ((fun m -> m.Value) >> (function 
+                | "L" -> Turn Left
+                | "R" -> Turn Right
+                | n -> Move(int n)))
 
-let turnToNumber t = 
-    match t with
-    | "R" -> 1
-    | "L" -> (-1)
-    | _ -> failwith "Invalid direction to turn to."
+let newPos state distance = 
+    match state with
+    | { pos = x, y; heading = n, m } -> { state with pos = x + (n * distance), y + (m * distance) }
 
-let newCoords heading n (x, y) = 
-    match heading with
-    | 1 -> x, y + n
-    | 2 -> x + n, y
-    | 3 -> x, y - n
-    | 4 | 0 -> x - n, y
-    | _ -> failwith "Invalid direction to walk in."
-
-let rec findBaseWithCoords d facing coords = 
-    let x, y = coords
-    match d with
-    | (h : string) :: t -> 
-        let turn', walk = h.Substring(0, 1), Convert.ToDouble(h.Substring(1))
-        let turn = turnToNumber turn'
-        let newFacing = (facing + turn |> abs) % 4 // feel like the problem is here in the modulos. 
-        findBaseWithCoords t newFacing (newCoords newFacing walk (x, y))
-    | [] -> abs (x) + abs (y)
+let findBaseWithCoords state d = 
+    Seq.fold (fun acc x -> 
+        match x with
+        | Turn Left -> { acc with heading = (rotate acc.heading Left) }
+        | Turn Right -> { acc with heading = (rotate acc.heading Right) }
+        | Move n -> (newPos acc n)) state d
 
 let findBase d = 
-    let origin = 0.0, 0.0 // the starting point to which all distance is relative
-    findBaseWithCoords d 1 origin
+    let finalState = findBaseWithCoords initState d
+    let x, y = finalState.pos
+    abs (x) + abs (y)
 
-let exThree = [ "R5"; "L5"; "R5"; "R3" ]
-let exTwo = [ "R2"; "L3" ]
-let exOne = [ "R2"; "R2"; "R2" ]
+let exOne = 
+    seq [ Turn Right
+          Move 2
+          Turn Left
+          Move 3 ]
 
-[ exOne; exTwo; exThree; directions ]
-|> List.map findBase
-|> printfn "%A"
-// gives the wrong answer for directions, 425 instead of 252.
+let exTwo = 
+    seq [ Turn Right
+          Move 2
+          Turn Right
+          Move 2
+          Turn Right
+          Move 2 ]
+
+let exThree = 
+    seq [ Turn Right
+          Move 5
+          Turn Left
+          Move 5
+          Turn Right
+          Move 5
+          Turn Right
+          Move 3 ]
+// gives the wrong answer for directions, 303 instead of 252.
